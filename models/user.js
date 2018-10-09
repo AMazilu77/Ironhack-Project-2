f7597895f0d108c8f9f8049e5d52c671ceca0f29
root <Amazilu77@gmail.com>
2018-10-08 22:22:44 -0400

    starting work on user schema, organizing user schema to collect saved numbers on each user profile so it can be pushed to database

--- models/user.js
+++ models/user.js
@@ -7,7 +7,16 @@ const userSchema = new Schema({
     facebookID: String,
     googleID: String,
     luckyNumbers: [],
-    numbersPlayed: [],
+    numbersPlayed: [{
+        pick2: {
+            ticket: [],
+            type: Number,
+            timestamps: {
+                    createdAt: "created_at"},
+        }
+    }],
+
+
     experiencePoints: {
         type: Number,
         default: 100
@@ -27,40 +36,5 @@ const userSchema = new Schema({
 });
 
 const User = mongoose.model("User", userSchema);
-
-module.exports = User;
-
-
-
-
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-
-// const userSchema = new Schema({
-//     username: String,
-//     password: String,
-//     facebookID: String,
-//     googleID: String,
-//     wishes: Number,
-//     luckyNumbers: Number,
-//     numbersPlayed: {
-//         type: [Number]
-//     },
-
-// }, {
-//     timestamps: {
-//         createdAt: "created_at",
-//         updatedAt: "updated_at"
-//     },
-
-
-
-
-
-
-
-// });
-
-// const User = mongoose.model("User", userSchema);
 
 module.exports = User;
