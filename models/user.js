e030b2bfeb47e313092b127782c54c3604138cb2
Alexander Teodor-Mazilu <Amazilu77@gmail.com>
2018-10-09 22:17:14 -0400

    pick 2 schema working

--- models/user.js
+++ models/user.js
@@ -1,42 +1,43 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     username: String,
     password: String,
     facebookID: String,
     googleID: String,
     luckyNumbers: [],
-    // schema for each game user saves numbers for 
+    // schema for each game user saves numbers for
     numbersPlayed: {
-        // counter to tell how many sets of numbers you have saved
-        nextPick2: Number,
-        //    array of saved tickets 
-        pick2: [{
-            Ticket: Number,
-            SavedAt: Date,
-            pick2Numbers: [],
-        },],
-
-
+      // counter to tell how many sets of numbers you have saved
+      nextPick2: Number,
+      //    array of saved tickets
+      pick2: [
+        {
+          Ticket: Number,
+          SavedAt: Date,
+          pick2Numbers: []
+        }
+      ]
     },
     experiencePoints: {
-        type: Number,
-        default: 100
+      type: Number,
+      default: 100
     },
 
     wishes: {
-        type: Number,
-        default: 3
-    },
-
-
-}, {
-        timestamps: {
-            createdAt: "created_at",
-            updatedAt: "updated_at"
-        }
-    });
+      type: Number,
+      default: 3
+    }
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
